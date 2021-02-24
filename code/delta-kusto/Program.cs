369a981c2e1cf0acfb6d6ddd2ea44a8afb1f1179
vplauzon <vplauzon@vplauzon.com>
2021-02-24 14:42:20 -0500

    Add dependency to CommandLineParser

--- code/delta-kusto/Program.cs
+++ code/delta-kusto/Program.cs
@@ -6,6 +6,8 @@ namespace delta_kusto
     {
         static void Main(string[] args)
         {
+            //  Use CommandLineParser NuGet package to parse command line
+            //  See https://github.com/commandlineparser/commandline
             Console.WriteLine("Hello World!");
         }
     }
