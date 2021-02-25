a3a92d957bcf6101c5c2a399f20c5d290856d679
vplauzon <vplauzon@vplauzon.com>
2021-02-25 16:27:36 -0500

    CreateOrAlterFunctionWithLetsAndProperties

--- code/DeltaKustoLib/CommandModel/CreateFunctionCommand.cs
+++ code/DeltaKustoLib/CommandModel/CreateFunctionCommand.cs
@@ -13,14 +13,26 @@ namespace DeltaKustoLib.CommandModel
 
         public string FunctionBody { get; }
 
+        public string? Folder { get; }
+
+        public string? DocString { get; }
+
+        public bool? SkipValidation { get; }
+
         private CreateFunctionCommand(
             string databaseName,
             string functionName,
-            string functionBody)
+            string functionBody,
+            string? folder,
+            string? docString,
+            bool? skipValidation)
             : base(databaseName)
         {
             FunctionName = functionName;
             FunctionBody = functionBody;
+            Folder = folder;
+            DocString = docString;
+            SkipValidation = skipValidation;
         }
 
         public override bool Equals([AllowNull] CommandBase other)
@@ -29,12 +41,38 @@ namespace DeltaKustoLib.CommandModel
 
             return otherFunction != null
                 && otherFunction.FunctionName == FunctionName
-                && otherFunction.FunctionBody == FunctionBody;
+                && otherFunction.FunctionBody == FunctionBody
+                && otherFunction.Folder == Folder
+                && otherFunction.DocString == DocString
+                && otherFunction.SkipValidation == SkipValidation;
         }
 
         public override string ToScript()
         {
-            return $".create-or-alter function {FunctionName} {{ {FunctionBody} }}";
+            var builder = new StringBuilder();
+            var properties = new[]
+            {
+                Folder!=null ? $"folder=\"{Folder}\"" : null,
+                DocString!=null ? $"docstring=\"{DocString}\"" : null,
+                SkipValidation!=null ? $"skipvalidation=\"{SkipValidation}\"" : null
+            };
+            var nonEmptyProperties = properties.Where(p => p != null);
+
+            builder.Append(".create-or-alter function ");
+            if (nonEmptyProperties.Any())
+            {
+                builder.Append("with (");
+                builder.AppendJoin(", ", nonEmptyProperties);
+                builder.Append(") ");
+            }
+            builder.Append(FunctionName);
+            builder.Append(" {");
+            builder.AppendLine();
+            builder.Append(FunctionBody);
+            builder.AppendLine();
+            builder.Append("}");
+
+            return builder.ToString();
         }
 
         internal static CommandBase FromCode(
@@ -42,13 +80,14 @@ namespace DeltaKustoLib.CommandModel
             string script,
             CommandBlock commandBlock)
         {
-            var nameDeclarations = commandBlock.GetDescendants<NameDeclaration>();
+            var functionNameDeclarations = commandBlock.GetDescendants<NameDeclaration>(
+                n => n.NameInParent == "FunctionName");
             var functionBodies = commandBlock.GetDescendants<FunctionBody>();
 
-            if (nameDeclarations.Count < 1)
+            if (functionNameDeclarations.Count < 1)
             {
                 throw new DeltaException(
-                    "There should be at least one name declaration but there are none",
+                    "There should be at least one function name declaration but there are none",
                     script);
             }
             if (functionBodies.Count != 1)
@@ -58,17 +97,76 @@ namespace DeltaKustoLib.CommandModel
                     script);
             }
 
-            var nameDeclaration = nameDeclarations.First();
-            var functionName = nameDeclaration.Name.SimpleName;
+            var functionNameDeclaration = functionNameDeclarations.First();
+            var functionName = functionNameDeclaration.Name.SimpleName;
             var functionBody = functionBodies.First();
             var bodyText = script.Substring(
                 functionBody.OpenBrace.TextStart + 1,
-                functionBody.CloseBrace.TextStart - functionBody.OpenBrace.TextStart -1 );
+                functionBody.CloseBrace.TextStart - functionBody.OpenBrace.TextStart - 1);
+            var folder = GetPropertyValue(script, "folder", commandBlock);
+            var docString = GetPropertyValue(script, "docstring", commandBlock);
+            var skipValidation = GetSkipValidation(script, commandBlock);
 
             return new CreateFunctionCommand(
                 databaseName,
                 functionName,
-                bodyText.Trim());
+                bodyText.Trim(),
+                folder,
+                docString,
+                skipValidation);
+        }
+
+        private static bool? GetSkipValidation(string script, CommandBlock commandBlock)
+        {
+            var skipValidationText = GetPropertyValue(script, "skipvalidation", commandBlock);
+
+            if (skipValidationText == null)
+            {
+                return null;
+            }
+            else
+            {
+                if (skipValidationText.ToLower() != "true"
+                && skipValidationText.ToLower() != "false")
+                {
+                    throw new DeltaException($"skipvalidation must be 'true' or 'false', it can't be '{skipValidationText}'");
+                }
+
+                var skipValidation = bool.Parse(skipValidationText);
+
+                return skipValidation;
+            }
+        }
+
+        private static string? GetPropertyValue(
+            string script,
+            string propertyName,
+            CommandBlock commandBlock)
+        {
+            var propertyNameDeclaration = commandBlock.GetDescendants<NameDeclaration>(
+                n => n.NameInParent == "PropertyName" && n.SimpleName == propertyName)
+                .FirstOrDefault();
+
+            if (propertyNameDeclaration == null)
+            {
+                return null;
+            }
+            else
+            {
+                var literalExpression = propertyNameDeclaration
+                    .Parent
+                    .GetDescendants<LiteralExpression>()
+                    .FirstOrDefault();
+
+                if (literalExpression == null)
+                {
+                    throw new DeltaException(
+                        $"Can't find literal expression for {propertyName}",
+                        script);
+                }
+
+                return (string)literalExpression.LiteralValue;
+            }
         }
     }
 }
--- code/DeltaKustoUnitTest/CommandParsing/CreateFunctionTest.cs
+++ code/DeltaKustoUnitTest/CommandParsing/CreateFunctionTest.cs
@@ -47,5 +47,27 @@ namespace DeltaKustoUnitTest.CommandParsing
             Assert.Equal(name, createFunctionCommand.FunctionName);
             Assert.Equal(body, createFunctionCommand.FunctionBody);
         }
+
+        [Fact]
+        public void CreateOrAlterFunctionWithLetsAndProperties()
+        {
+            var name = "MyFunction4";
+            var body = "let limitVar = 100; let result = MyFunction(limitVar); result";
+            var folder = "Demo";
+            var docString = "Function calling other function";
+            var skipValidation = true;
+            var command = ParseOneCommand(
+                $".create-or-alter function with (folder = \"{folder}\", docstring = \"{docString}\", skipvalidation = \"{skipValidation}\") {name} () {{ {body} }}");
+
+            Assert.IsType<CreateFunctionCommand>(command);
+
+            var createFunctionCommand = (CreateFunctionCommand)command;
+
+            Assert.Equal(name, createFunctionCommand.FunctionName);
+            Assert.Equal(body, createFunctionCommand.FunctionBody);
+            Assert.Equal(folder, createFunctionCommand.Folder);
+            Assert.Equal(docString, createFunctionCommand.DocString);
+            Assert.Equal(skipValidation, createFunctionCommand.SkipValidation);
+        }
     }
 }
