b8b717c97838cd236eb2e7405d1b7dadfc636dec
vplauzon <vplauzon@vplauzon.com>
2021-02-28 20:05:58 -0500

    .

---
+++ code/DeltaKustoLib/CommandModel/DropFunctionCommand.cs
@@ -0,0 +1,41 @@
+﻿using Kusto.Language.Syntax;
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+
+namespace DeltaKustoLib.CommandModel
+{
+    internal class DropFunctionCommand : CommandBase
+    {
+        public string FunctionName { get; }
+
+        public DropFunctionCommand(string functionName)
+        {
+            FunctionName = functionName;
+        }
+
+        internal static CommandBase FromCode(
+            string databaseName,
+            CustomCommand customCommand)
+        {
+            throw new NotSupportedException(".drop function not supported in this context");
+        }
+
+        public override bool Equals(CommandBase? other)
+        {
+            var otherFunction = other as CreateFunctionCommand;
+            var areEqualed = otherFunction != null
+                && otherFunction.FunctionName == FunctionName;
+
+            return areEqualed;
+        }
+
+        public override string ToScript()
+        {
+            return $".drop function ['{FunctionName}']";
+        }
+    }
+}
