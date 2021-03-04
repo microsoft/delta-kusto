65512214eb3f073727f8581b8991fa2a104ecaba
vplauzon <vplauzon@vplauzon.com>
2021-03-04 13:03:18 -0500

    .

--- code/DeltaKustoIntegration/Parameterization/ServicePrincipalLoginParameterization.cs
+++ code/DeltaKustoIntegration/Parameterization/ServicePrincipalLoginParameterization.cs
@@ -5,12 +5,18 @@ namespace DeltaKustoIntegration.Parameterization
 {
     public class ServicePrincipalLoginParameterization
     {
-        public string? ClientId { get; set; }
+        public string? TenantId { get; set; }
         
+        public string? ClientId { get; set; }
+
         public string? Secret { get; set; }
 
         internal void Validate()
         {
+            if (TenantId == null)
+            {
+                throw new DeltaException("'tenantId' must be populated in login");
+            }
             if (ClientId == null)
             {
                 throw new DeltaException("'clientId' must be populated in login");
