bc7b4acde4991d28ab4a4d1134925279f9191b57
vplauzon <vplauzon@vplauzon.com>
2021-03-07 12:20:37 -0500

    Add ParameterOverrideHelperTest and one test

---
+++ code/DeltaKustoUnitTest/ParameterOverrideHelperTest.cs
@@ -0,0 +1,25 @@
+﻿using DeltaKustoIntegration.Parameterization;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace DeltaKustoUnitTest
+{
+    public class ParameterOverrideHelperTest
+    {
+        [Fact]
+        public void TestSingleProperty()
+        {
+            var main = new MainParameterization();
+            var before = main.SendTelemetryOptIn;
+            var value = (!before).ToString().ToLower();
+
+            ParameterOverrideHelper.InplaceOverride(
+                main,
+                $"[{{\"path\" : \"sendTelemetryOptIn\", \"value\":{value} }}]");
+
+            Assert.Equal(!before, main.SendTelemetryOptIn);
+        }
+    }
+}
