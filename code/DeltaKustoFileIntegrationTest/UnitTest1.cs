2e5af2f191eed0d39cbcf36f4bb9f6c27639f488
vplauzon <vplauzon@vplauzon.com>
2021-03-08 09:59:28 -0500

    Add DeltaKustoFileIntegrationTest project

---
+++ code/DeltaKustoFileIntegrationTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace DeltaKustoFileIntegrationTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
