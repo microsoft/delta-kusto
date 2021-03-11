00effa1bbed7a4f93515b5f8ce669b2360adf960
vplauzon <vplauzon@vplauzon.com>
2021-03-11 18:21:42 -0500

    2 more tests

--- code/DeltaKustoAdxIntegrationTest/Functions/AdxToFile/FunctionsAdxToFileTest.cs
+++ code/DeltaKustoAdxIntegrationTest/Functions/AdxToFile/FunctionsAdxToFileTest.cs
@@ -16,11 +16,37 @@ namespace DeltaKustoAdxIntegrationTest
         public async Task FromEmptyDbToEmptyScript()
         {
             var parameters = await RunParametersAsync(
-                "Functions/AdxToFile/FromEmptyDb/delta-params.json");
+                "Functions/AdxToFile/FromEmptyDbToEmptyScript/delta-params.json");
             var outputPath = parameters.Jobs!.First().Value.Action!.FilePath!;
             var commands = await LoadScriptAsync(outputPath);
 
             Assert.Empty(commands);
+        }
+
+        [Fact]
+        public async Task FromEmptyDbToOneFunction()
+        {
+            var parameters = await RunParametersAsync(
+                "Functions/AdxToFile/FromEmptyDbToOneFunction/delta-params.json");
+            var outputPath = parameters.Jobs!.First().Value.Action!.FilePath!;
+            var inputPath = parameters.Jobs!.First().Value.Target!.Scripts!.First().FilePath!;
+            var inputCommands = await LoadScriptAsync(inputPath);
+            var outputCommands = await LoadScriptAsync(outputPath);
+
+            Assert.True(inputCommands.SequenceEqual(outputCommands));
+        }
+
+        [Fact]
+        public async Task FromEmptyDbToTwoFunctions()
+        {
+            var parameters = await RunParametersAsync(
+                "Functions/AdxToFile/FromEmptyDbToTwoFunctions/delta-params.json");
+            var inputPath = parameters.Jobs!.First().Value.Target!.Scripts!.First().FilePath!;
+            var outputPath = parameters.Jobs!.First().Value.Action!.FilePath!;
+            var inputCommands = await LoadScriptAsync(inputPath);
+            var outputCommands = await LoadScriptAsync(outputPath);
+
+            Assert.True(inputCommands.SequenceEqual(outputCommands));
         }
     }
 }
