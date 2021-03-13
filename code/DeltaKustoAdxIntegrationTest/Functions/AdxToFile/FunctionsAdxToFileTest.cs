2227bf2c5b258e0c6d1600baf3d0ba0f7e614c73
vplauzon <vplauzon@vplauzon.com>
2021-03-13 02:02:14 -0500

    Make loop generic

--- code/DeltaKustoAdxIntegrationTest/Functions/AdxToFile/FunctionsAdxToFileTest.cs
+++ code/DeltaKustoAdxIntegrationTest/Functions/AdxToFile/FunctionsAdxToFileTest.cs
@@ -20,41 +20,49 @@ namespace DeltaKustoAdxIntegrationTest
         [Fact]
         public async Task GenericAdxToFile()
         {
+            await LoopThroughStateFilesAsync(async (fromFile, toFile) =>
+            {
+                await PrepareCurrentAsync(fromFile);
+
+                var outputPath = "outputs/functions/adx-to-file/"
+                    + Path.GetFileNameWithoutExtension(fromFile)
+                    + "_"
+                    + Path.GetFileNameWithoutExtension(toFile)
+                    + ".kql";
+                var overrides = new[]
+                {
+                        ("jobs.main.target.scripts", (object) new[]{ new { filePath = toFile } }),
+                        ("jobs.main.action.filePath", outputPath)
+                    };
+                var parameters = await RunParametersAsync(
+                    "Functions/AdxToFile/delta-params.json",
+                    overrides);
+                var outputCommands = await LoadScriptAsync(outputPath);
+                var targetCommands = CommandBase.FromScript(
+                    await File.ReadAllTextAsync(toFile));
+                var finalCommands = await ApplyCommandsToCurrent(outputCommands);
+
+                Assert.True(
+                    finalCommands.SequenceEqual(targetCommands),
+                    $"From {fromFile} to {toFile}");
+            });
+        }
+
+        private async Task LoopThroughStateFilesAsync(Func<string, string, Task> loopFunction)
+        {
             var stateFiles = Directory.GetFiles("Functions/States");
 
             foreach (var fromFile in stateFiles)
             {
                 foreach (var toFile in stateFiles)
                 {
-                    await PrepareCurrentAsync(fromFile);
-
-                    var outputPath = "outputs/functions/adx-to-file/"
-                        + Path.GetFileNameWithoutExtension(fromFile)
-                        + "_"
-                        + Path.GetFileNameWithoutExtension(toFile)
-                        + ".kql";
-                    var overrides = new[]
-                    {
-                        ("jobs.main.target.scripts", (object) new[]{ new { filePath = toFile } }),
-                        ("jobs.main.action.filePath", outputPath)
-                    };
-                    var parameters = await RunParametersAsync(
-                        "Functions/AdxToFile/delta-params.json",
-                        overrides);
-                    var outputCommands = await LoadScriptAsync(outputPath);
-                    var targetCommands = CommandBase.FromScript(
-                        await File.ReadAllTextAsync(toFile));
-                    var finalCommands = await ApplyCommandsToCurrent(outputCommands);
-
-                    Assert.True(
-                        finalCommands.SequenceEqual(targetCommands),
-                        $"From {fromFile} to {toFile}");
+                    await loopFunction(fromFile, toFile);
                 }
             }
         }
 
         private async Task<IImmutableList<CommandBase>> ApplyCommandsToCurrent(
-            IEnumerable<CommandBase> outputCommands)
+    IEnumerable<CommandBase> outputCommands)
         {
             var gateway = CreateKustoManagementGateway(true);
             var dbProvider = (IDatabaseProvider)new KustoDatabaseProvider(gateway);
