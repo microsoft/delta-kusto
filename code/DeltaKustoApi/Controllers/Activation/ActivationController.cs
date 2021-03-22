c766bae080de8a8db7cfaaa172ccc92d48938ae7
vplauzon <vplauzon@vplauzon.com>
2021-03-22 11:07:22 -0400

    Create DeltaKustoApi project

---
+++ code/DeltaKustoApi/Controllers/Activation/ActivationController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DeltaKustoApi.Controllers.Activation
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ActivationController : ControllerBase
+    {
+        private readonly ILogger<ActivationController> _logger;
+
+        public ActivationController(ILogger<ActivationController> logger)
+        {
+            _logger = logger;
+        }
+    }
+}
---
+++ code/DeltaKustoApi/Controllers/Root/RootController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DeltaKustoApi.Controllers.Root
+{
+    [ApiController]
+    [Route("/")]
+    public class RootController : ControllerBase
+    {
+        private readonly ILogger<RootController> _logger;
+
+        public RootController(ILogger<RootController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public RootInfo Get()
+        {
+            var info = new RootInfo
+            {
+                ApiVersion = ApiVersion.FullVersion
+            };
+
+            return info;
+        }
+    }
+}
