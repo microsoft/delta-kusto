4c30c256e8b26237f06e1b5bb09a4b5c9b7181b4
vplauzon <vplauzon@vplauzon.com>
2021-04-04 15:35:22 -0400

    Forget TracerTimer file

---
+++ code/DeltaKustoLib/TracerTimer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeltaKustoLib
+{
+    public class TracerTimer
+    {
+        private readonly ITracer _tracer;
+        private readonly Stopwatch _watch = new Stopwatch();
+
+        public TracerTimer(ITracer tracer)
+        {
+            _tracer = tracer;
+            _watch.Start();
+        }
+
+        public void WriteTime(bool isVerbose, string text)
+        {
+            _tracer.WriteLine(isVerbose, $"{text} - {_watch.Elapsed}");
+        }
+    }
+}
