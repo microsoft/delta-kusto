b4cecc74d76829ca4426fe878ec548c34b669ab3
vplauzon <vplauzon@vplauzon.com>
2021-04-05 08:39:47 -0400

    Increase time out for APIs

--- code/delta-kusto/TimeOuts.cs
+++ code/delta-kusto/TimeOuts.cs
@@ -4,7 +4,7 @@ namespace delta_kusto
 {
     internal class TimeOuts
     {
-        public static TimeSpan API = TimeSpan.FromSeconds(5);
+        public static TimeSpan API = TimeSpan.FromSeconds(8);
         
         public static TimeSpan FILE = TimeSpan.FromSeconds(.5);
 
