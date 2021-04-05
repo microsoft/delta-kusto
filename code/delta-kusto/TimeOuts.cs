2e802b4139dc601b0046db2bd574732613c96c07
vplauzon <vplauzon@vplauzon.com>
2021-04-05 09:07:50 -0400

    Bump time outs

--- code/delta-kusto/TimeOuts.cs
+++ code/delta-kusto/TimeOuts.cs
@@ -4,12 +4,12 @@ namespace delta_kusto
 {
     internal class TimeOuts
     {
-        public static TimeSpan API = TimeSpan.FromSeconds(8);
+        public static TimeSpan API = TimeSpan.FromSeconds(10);
         
         public static TimeSpan FILE = TimeSpan.FromSeconds(.5);
 
-        public static TimeSpan RETRIEVE_DB = TimeSpan.FromSeconds(5);
+        public static TimeSpan RETRIEVE_DB = TimeSpan.FromSeconds(15);
         
-        public static TimeSpan ACTION = TimeSpan.FromSeconds(20);
+        public static TimeSpan ACTION = TimeSpan.FromSeconds(40);
     }
 }
