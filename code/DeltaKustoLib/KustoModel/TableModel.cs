4ffab1be0beb1d3df16556353bd20b399675e734
vplauzon <vplauzon@vplauzon.com>
2021-04-12 18:51:34 -0400

    Create TableModel

---
+++ code/DeltaKustoLib/KustoModel/TableModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeltaKustoLib.KustoModel
+{
+    public class TableModel
+    {
+        public string TableName { get; }
+
+        public IImmutableList<ColumnModel> Columns { get; }
+
+        public string? Folder { get; }
+
+        public string? DocString { get; }
+
+        private TableModel(
+            string tableName,
+            IEnumerable<ColumnModel> columns,
+            string? folder,
+            string? docString)
+        {
+            TableName = tableName;
+            Columns = columns.ToImmutableArray();
+            Folder = string.IsNullOrEmpty(folder) ? null : folder;
+            DocString = string.IsNullOrEmpty(docString) ? null : docString;
+        }
+    }
+}
