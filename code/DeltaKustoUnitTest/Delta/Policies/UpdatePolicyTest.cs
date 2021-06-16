6ab6ddd986a5bf042bd9dd716aed4c59c3bde9b3
vplauzon <vplauzon@vplauzon.com>
2021-06-16 15:16:19 -0400

    Move UpdatePolicyTest.cs

--- code/DeltaKustoUnitTest/Delta/Policies/UpdatePolicyTest.cs
+++ code/DeltaKustoUnitTest/Delta/Policies/UpdatePolicyTest.cs
@@ -6,7 +6,7 @@ using System.Collections.Generic;
 using System.Text;
 using Xunit;
 
-namespace DeltaKustoUnitTest.Delta
+namespace DeltaKustoUnitTest.Delta.Policies
 {
     public class UpdatePolicyTest : ParsingTestBase
     {
