ecfdaf15572321d7c3e7de9205035abf87b55b41
vplauzon <vplauzon@vplauzon.com>
2021-06-19 07:15:48 -0400

    Add File Db tests

---
+++ code/DeltaKustoFileIntegrationTest/Policies/Caching/CachingPolicyDatabaseTest.cs
@@ -0,0 +1,86 @@
+﻿using DeltaKustoLib.CommandModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace DeltaKustoFileIntegrationTest.Policies.Caching
+{
+    public class CachingPolicyDatabaseTest : IntegrationTestBase
+    {
+        [Fact]
+        public async Task NoneToOne()
+        {
+            var paramPath = "Policies/Caching/Database/NoneToOne/delta-params.yaml";
+            var parameters = await RunParametersAsync(paramPath);
+            var outputPath = parameters.Jobs!.First().Value.Action!.FilePath!;
+            var outputCommands = await LoadScriptAsync(paramPath, outputPath);
+
+            Assert.Single(outputCommands);
+
+            var policyCommand = outputCommands
+                .Where(c => c is AlterCachingPolicyCommand)
+                .Cast<AlterCachingPolicyCommand>()
+                .FirstOrDefault();
+
+            Assert.NotNull(policyCommand);
+            Assert.Equal(EntityType.Database, policyCommand!.EntityType);
+            Assert.Equal("mydb", policyCommand!.EntityName.Name);
+            Assert.Equal("12h", policyCommand!.DurationText);
+        }
+
+        [Fact]
+        public async Task OneToNone()
+        {
+            var paramPath = "Policies/Caching/Database/OneToNone/delta-params.yaml";
+            var parameters = await RunParametersAsync(paramPath);
+            var outputPath = parameters.Jobs!.First().Value.Action!.FilePath!;
+            var outputCommands = await LoadScriptAsync(paramPath, outputPath);
+
+            Assert.Single(outputCommands);
+
+            var policyCommand = outputCommands
+                .Where(c => c is DeleteCachingPolicyCommand)
+                .Cast<DeleteCachingPolicyCommand>()
+                .FirstOrDefault();
+
+            Assert.NotNull(policyCommand);
+            Assert.Equal(EntityType.Database, policyCommand!.EntityType);
+            Assert.Equal("my-db", policyCommand!.EntityName.Name);
+        }
+
+        [Fact]
+        public async Task OneToOne()
+        {
+            var paramPath = "Policies/Caching/Database/OneToOneNoChange/delta-params.yaml";
+            var parameters = await RunParametersAsync(paramPath);
+            var outputPath = parameters.Jobs!.First().Value.Action!.FilePath!;
+            var outputCommands = await LoadScriptAsync(paramPath, outputPath);
+
+            Assert.Empty(outputCommands);
+        }
+
+        [Fact]
+        public async Task OneToOneWithChange()
+        {
+            var paramPath = "Policies/Caching/Database/OneToOneWithChange/delta-params.yaml";
+            var parameters = await RunParametersAsync(paramPath);
+            var outputPath = parameters.Jobs!.First().Value.Action!.FilePath!;
+            var outputCommands = await LoadScriptAsync(paramPath, outputPath);
+
+            Assert.Single(outputCommands);
+
+            var policyCommand = outputCommands
+                .Where(c => c is AlterCachingPolicyCommand)
+                .Cast<AlterCachingPolicyCommand>()
+                .FirstOrDefault();
+
+            Assert.NotNull(policyCommand);
+            Assert.Equal(EntityType.Database, policyCommand!.EntityType);
+            Assert.Equal("my db", policyCommand!.EntityName.Name);
+            Assert.Equal("10d", policyCommand!.DurationText);
+        }
+    }
+}
