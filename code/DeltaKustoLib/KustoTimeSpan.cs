60d14882e7a2c953f24ad61b2c617e8c980af7c3
vplauzon <vplauzon@vplauzon.com>
2021-06-21 09:25:07 -0400

    Add KustoTimeSpan

---
+++ code/DeltaKustoLib/KustoTimeSpan.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeltaKustoLib
+{
+    public class KustoTimeSpan
+    {
+        public KustoTimeSpan(TimeSpan? duration)
+        {
+        }
+
+        public TimeSpan? Duration { get; }
+
+        public override string ToString()
+        {
+            if (Duration == null)
+            {
+                return "time(null)";
+            }
+            else
+            {
+                TimeSpan duration = Duration.Value;
+
+                if (IsSimpleDecimal(duration.TotalDays))
+                {
+                    return MakeLiteral(duration.TotalDays, "d");
+                }
+                else if (IsSimpleDecimal(duration.Hours))
+                {
+                    return MakeLiteral(duration.Hours, "h");
+                }
+                else if (IsSimpleDecimal(duration.Minutes))
+                {
+                    return MakeLiteral(duration.Minutes, "m");
+                }
+                else if (IsSimpleDecimal(duration.Seconds))
+                {
+                    return MakeLiteral(duration.Seconds, "s");
+                }
+                else if (IsSimpleDecimal(duration.Milliseconds))
+                {
+                    return MakeLiteral(duration.Milliseconds, "ms");
+                }
+                else
+                {
+                    var time = duration.ToString("G");
+
+                    return $"time({time})";
+                }
+            }
+        }
+
+        private static string MakeLiteral(double number, string suffix)
+        {
+            if (number == (int)number)
+            {
+                return number + suffix;
+            }
+            else
+            {
+                return $"timespan({number}{suffix})";
+            }
+        }
+
+        private static bool IsSimpleDecimal(double number)
+        {
+            return number * 10 == (int)(number * 10);
+        }
+    }
+}
