64b641ea7fc931b8ccc73589d8c92626586bb21f
vplauzon <vplauzon@vplauzon.com>
2021-07-09 16:57:06 -0400

    Add delete of sharding + merge

---
+++ code/DeltaKustoLib/CommandModel/Policies/DeleteShardingPolicyCommand.cs
@@ -0,0 +1,84 @@
+﻿using Kusto.Language.Syntax;
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace DeltaKustoLib.CommandModel.Policies
+{
+    /// <summary>
+    /// Models <see cref="https://docs.microsoft.com/en-us/azure/data-explorer/kusto/management/sharding-policy#delete-policy"/>
+    /// </summary>
+    public class DeleteShardingPolicyCommand : CommandBase
+    {
+        public EntityType EntityType { get; }
+
+        public EntityName EntityName { get; }
+
+        public override string CommandFriendlyName => ".delete <entity> policy sharding";
+
+        public DeleteShardingPolicyCommand(EntityType entityType, EntityName entityName)
+        {
+            if (entityType != EntityType.Database && entityType != EntityType.Table)
+            {
+                throw new NotSupportedException(
+                    $"Entity type {entityType} isn't supported in this context");
+            }
+            EntityType = entityType;
+            EntityName = entityName;
+        }
+
+        internal static CommandBase FromCode(SyntaxElement rootElement)
+        {
+            var entityKinds = rootElement
+                .GetDescendants<SyntaxElement>(s => s.Kind == SyntaxKind.TableKeyword
+                || s.Kind == SyntaxKind.DatabaseKeyword)
+                .Select(s => s.Kind);
+
+            if (!entityKinds.Any())
+            {
+                throw new DeltaException("Delete sharding policy requires to act on a table or database (cluster isn't supported)");
+            }
+            var entityKind = entityKinds.First();
+            var entityType = entityKind == SyntaxKind.TableKeyword
+                ? EntityType.Table
+                : EntityType.Database;
+            var entityName = rootElement.GetFirstDescendant<NameReference>();
+
+            return new DeleteShardingPolicyCommand(entityType, EntityName.FromCode(entityName.Name));
+        }
+
+        public override bool Equals(CommandBase? other)
+        {
+            var otherFunction = other as DeleteShardingPolicyCommand;
+            var areEqualed = otherFunction != null
+                && otherFunction.EntityType.Equals(EntityType)
+                && otherFunction.EntityName.Equals(EntityName);
+
+            return areEqualed;
+        }
+
+        public override string ToScript(ScriptingContext? context)
+        {
+            var builder = new StringBuilder();
+
+            builder.Append(".delete ");
+            builder.Append(EntityType == EntityType.Table ? "table" : "database");
+            builder.Append(" ");
+            if (EntityType == EntityType.Database && context?.CurrentDatabaseName != null)
+            {
+                builder.Append(context.CurrentDatabaseName.ToScript());
+            }
+            else
+            {
+                builder.Append(EntityName.ToScript());
+            }
+            builder.Append(" policy sharding");
+
+            return builder.ToString();
+        }
+    }
+}
