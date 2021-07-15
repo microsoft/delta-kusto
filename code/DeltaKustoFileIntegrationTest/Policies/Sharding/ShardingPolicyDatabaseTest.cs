19441916de614c384772193a0706fb3ae44986a7
vplauzon <vplauzon@vplauzon.com>
2021-07-14 20:23:42 -0400

    Add ShardingPolicy*Test

---
+++ code/DeltaKustoFileIntegrationTest/Policies/Sharding/ShardingPolicyDatabaseTest.cs
@@ -0,0 +1,98 @@
+﻿using DeltaKustoLib.CommandModel;
+using DeltaKustoLib.CommandModel.Policies;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace DeltaKustoFileIntegrationTest.Policies.Sharding
+{
+    public class ShardingPolicyDatabaseTest : IntegrationTestBase
+    {
+        #region Inner types
+        private record ShardingPolicy
+        {
+            public int MaxRowCount { get; init; }
+        }
+        #endregion
+
+        [Fact]
+        public async Task NoneToOne()
+        {
+            var paramPath = "Policies/Sharding/Database/NoneToOne/delta-params.yaml";
+            var parameters = await RunParametersAsync(paramPath);
+            var outputPath = parameters.Jobs!.First().Value.Action!.FilePath!;
+            var outputCommands = await LoadScriptAsync(paramPath, outputPath);
+
+            Assert.Single(outputCommands);
+
+            var policyCommand = outputCommands
+                .Where(c => c is AlterShardingPolicyCommand)
+                .Cast<AlterShardingPolicyCommand>()
+                .FirstOrDefault();
+
+            Assert.NotNull(policyCommand);
+            Assert.Equal(EntityType.Database, policyCommand!.EntityType);
+            Assert.Equal("mydb", policyCommand!.EntityName.Name);
+            Assert.Equal(
+                750000,
+                policyCommand!.DeserializePolicy<ShardingPolicy>().MaxRowCount);
+        }
+
+        [Fact]
+        public async Task OneToNone()
+        {
+            var paramPath = "Policies/Sharding/Database/OneToNone/delta-params.yaml";
+            var parameters = await RunParametersAsync(paramPath);
+            var outputPath = parameters.Jobs!.First().Value.Action!.FilePath!;
+            var outputCommands = await LoadScriptAsync(paramPath, outputPath);
+
+            Assert.Single(outputCommands);
+
+            var policyCommand = outputCommands
+                .Where(c => c is DeleteShardingPolicyCommand)
+                .Cast<DeleteShardingPolicyCommand>()
+                .FirstOrDefault();
+
+            Assert.NotNull(policyCommand);
+            Assert.Equal(EntityType.Database, policyCommand!.EntityType);
+            Assert.Equal("mydb", policyCommand!.EntityName.Name);
+        }
+
+        [Fact]
+        public async Task OneToOne()
+        {
+            var paramPath = "Policies/Sharding/Database/OneToOneNoChange/delta-params.yaml";
+            var parameters = await RunParametersAsync(paramPath);
+            var outputPath = parameters.Jobs!.First().Value.Action!.FilePath!;
+            var outputCommands = await LoadScriptAsync(paramPath, outputPath);
+
+            Assert.Empty(outputCommands);
+        }
+
+        [Fact]
+        public async Task OneToOneWithChange()
+        {
+            var paramPath = "Policies/Sharding/Database/OneToOneWithChange/delta-params.yaml";
+            var parameters = await RunParametersAsync(paramPath);
+            var outputPath = parameters.Jobs!.First().Value.Action!.FilePath!;
+            var outputCommands = await LoadScriptAsync(paramPath, outputPath);
+
+            Assert.Single(outputCommands);
+
+            var policyCommand = outputCommands
+                .Where(c => c is AlterShardingPolicyCommand)
+                .Cast<AlterShardingPolicyCommand>()
+                .FirstOrDefault();
+
+            Assert.NotNull(policyCommand);
+            Assert.Equal(EntityType.Database, policyCommand!.EntityType);
+            Assert.Equal("mydb", policyCommand!.EntityName.Name);
+            Assert.Equal(
+               1000000,
+               policyCommand!.DeserializePolicy<ShardingPolicy>().MaxRowCount);
+        }
+    }
+}
---
+++ code/DeltaKustoFileIntegrationTest/Policies/Sharding/ShardingPolicyTableTest.cs
@@ -0,0 +1,98 @@
+﻿using DeltaKustoLib.CommandModel;
+using DeltaKustoLib.CommandModel.Policies;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace DeltaKustoFileIntegrationTest.Policies.Sharding
+{
+    public class ShardingPolicyTableTest : IntegrationTestBase
+    {
+        #region Inner types
+        private record ShardingPolicy
+        {
+            public int MaxRowCount { get; init; }
+        }
+        #endregion
+
+        [Fact]
+        public async Task NoneToOne()
+        {
+            var paramPath = "Policies/Sharding/Table/NoneToOne/delta-params.yaml";
+            var parameters = await RunParametersAsync(paramPath);
+            var outputPath = parameters.Jobs!.First().Value.Action!.FilePath!;
+            var outputCommands = await LoadScriptAsync(paramPath, outputPath);
+
+            Assert.Single(outputCommands);
+
+            var policyCommand = outputCommands
+                .Where(c => c is AlterShardingPolicyCommand)
+                .Cast<AlterShardingPolicyCommand>()
+                .FirstOrDefault();
+
+            Assert.NotNull(policyCommand);
+            Assert.Equal(EntityType.Table, policyCommand!.EntityType);
+            Assert.Equal("my-table", policyCommand!.EntityName.Name);
+            Assert.Equal(
+                750000,
+                policyCommand!.DeserializePolicy<ShardingPolicy>().MaxRowCount);
+        }
+
+        [Fact]
+        public async Task OneToNone()
+        {
+            var paramPath = "Policies/Sharding/Table/OneToNone/delta-params.yaml";
+            var parameters = await RunParametersAsync(paramPath);
+            var outputPath = parameters.Jobs!.First().Value.Action!.FilePath!;
+            var outputCommands = await LoadScriptAsync(paramPath, outputPath);
+
+            Assert.Single(outputCommands);
+
+            var policyCommand = outputCommands
+                .Where(c => c is DeleteShardingPolicyCommand)
+                .Cast<DeleteShardingPolicyCommand>()
+                .FirstOrDefault();
+
+            Assert.NotNull(policyCommand);
+            Assert.Equal(EntityType.Table, policyCommand!.EntityType);
+            Assert.Equal("my-table", policyCommand!.EntityName.Name);
+        }
+
+        [Fact]
+        public async Task OneToOne()
+        {
+            var paramPath = "Policies/Sharding/Table/OneToOneNoChange/delta-params.yaml";
+            var parameters = await RunParametersAsync(paramPath);
+            var outputPath = parameters.Jobs!.First().Value.Action!.FilePath!;
+            var outputCommands = await LoadScriptAsync(paramPath, outputPath);
+
+            Assert.Empty(outputCommands);
+        }
+
+        [Fact]
+        public async Task OneToOneWithChange()
+        {
+            var paramPath = "Policies/Sharding/Table/OneToOneWithChange/delta-params.yaml";
+            var parameters = await RunParametersAsync(paramPath);
+            var outputPath = parameters.Jobs!.First().Value.Action!.FilePath!;
+            var outputCommands = await LoadScriptAsync(paramPath, outputPath);
+
+            Assert.Single(outputCommands);
+
+            var policyCommand = outputCommands
+                .Where(c => c is AlterShardingPolicyCommand)
+                .Cast<AlterShardingPolicyCommand>()
+                .FirstOrDefault();
+
+            Assert.NotNull(policyCommand);
+            Assert.Equal(EntityType.Table, policyCommand!.EntityType);
+            Assert.Equal("my-table", policyCommand!.EntityName.Name);
+            Assert.Equal(
+               1000000,
+               policyCommand!.DeserializePolicy<ShardingPolicy>().MaxRowCount);
+        }
+    }
+}
