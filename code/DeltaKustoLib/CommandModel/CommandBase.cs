2f439ae0c5baf7cd0e4b913119cd8af49c591f90
vplauzon <vplauzon@vplauzon.com>
2021-07-30 16:17:24 -0400

    Re-integrate caching policy once updated kql.language

--- code/DeltaKustoLib/CommandModel/CommandBase.cs
+++ code/DeltaKustoLib/CommandModel/CommandBase.cs
@@ -141,14 +141,12 @@ namespace DeltaKustoLib.CommandModel
                         return AlterAutoDeletePolicyCommand.FromCode(commandBlock);
                     case "DeleteTablePolicyAutoDelete":
                         return DeleteAutoDeletePolicyCommand.FromCode(commandBlock);
-                        /*
                     case "AlterDatabasePolicyCaching":
                     case "AlterTablePolicyCaching":
                         return AlterCachingPolicyCommand.FromCode(commandBlock);
                     case "DeleteDatabasePolicyCaching":
                     case "DeleteTablePolicyCaching":
                         return DeleteCachingPolicyCommand.FromCode(commandBlock);
-                        */
                     case "AlterDatabasePolicyIngestionBatching":
                     case "AlterTablePolicyIngestionBatching":
                         return AlterIngestionBatchingPolicyCommand.FromCode(commandBlock);
