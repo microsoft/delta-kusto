51f6c2712ef85ee6c7817291ae5f29e3c1d90528
vplauzon <vplauzon@vplauzon.com>
2021-07-30 18:13:03 -0400

    Add non-running auto_delete integration tests

---
+++ code/DeltaKustoAdxIntegrationTest/Policies/AutoDelete/AutoDeletePolicyTest.cs
@@ -0,0 +1,17 @@
+using DeltaKustoIntegration.Database;
+using DeltaKustoLib.CommandModel;
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace DeltaKustoAdxIntegrationTest.Policies.AutoDelete
+{
+    public class AutoDeletePolicyTest //: AdxAutoIntegrationTestBase
+    {
+        //protected override string StatesFolderPath => "Policies/AutoDelete";
+    }
+}
