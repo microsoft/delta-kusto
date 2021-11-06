23f7e4b805e04e23c38df04a39bfcd01f4018580
vplauzon <vincent_lauzon@yahoo.com.au>
2021-11-06 18:02:00 -0400

    First draft of perf test

---
+++ code/perf-test/create-parameters-file.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+##########################################################################
+##  Create parameter file
+##
+##  Parameters:
+##
+##  1- DB Count
+##  2- Cluster URI
+##  3- Script file name (not .kql)
+
+dbCount=$1
+clusterUri=$2
+fileName=$3
+
+# echo "DB Count:  $dbCount"
+# echo "Cluster URI:  $clusterUri"
+
+#   Output header
+cat parameters-headers.yaml
+
+#   Loop for databases
+for i in $(seq -f "%08g" 1 $dbCount)
+do
+    #   Output for one db
+    sed "s/db-name/dN$i/g; s/cluster-uri/$clusterUri/g; s/script-file/$fileName/g" parameters-db.yaml
+done
