50c151482d093641991afd17b09089b9e94ec55a
vplauzon <vincent_lauzon@yahoo.com.au>
2021-11-18 14:03:03 -0500

    move things

--- code/DeltaKustoAdxIntegrationTest/FailIfDataLoss/FailIfDataLossTest.cs
+++ code/DeltaKustoAdxIntegrationTest/FailIfDataLoss/FailIfDataLossTest.cs
@@ -10,7 +10,7 @@ using System.Threading;
 using System.Threading.Tasks;
 using Xunit;
 
-namespace DeltaKustoAdxIntegrationTest
+namespace DeltaKustoAdxIntegrationTest.FailIfDataLoss
 {
     public class FailIfDataLossTest : AdxIntegrationTestBase
     {
--- code/DeltaKustoAdxIntegrationTest/GivenTokenProvider/GivenTokenProviderTest.cs
+++ code/DeltaKustoAdxIntegrationTest/GivenTokenProvider/GivenTokenProviderTest.cs
@@ -11,7 +11,7 @@ using System.Threading;
 using System.Threading.Tasks;
 using Xunit;
 
-namespace DeltaKustoAdxIntegrationTest
+namespace DeltaKustoAdxIntegrationTest.GivenTokenProvider
 {
     public class GivenTokenProviderTest : AdxIntegrationTestBase
     {
