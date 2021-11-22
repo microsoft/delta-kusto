8cef4a13ea5ec89948e8f8264d4f0ab2a204b964
vplauzon <vincent_lauzon@yahoo.com.au>
2021-11-22 09:27:22 -0500

    Add AlterPartitioningPolicyTest

---
+++ code/DeltaKustoUnitTest/CommandParsing/Policies/AlterPartitioningPolicyTest.cs
@@ -0,0 +1,83 @@
+﻿using DeltaKustoLib.CommandModel;
+using System;
+using System.Linq;
+using System.Text.Json;
+using Xunit;
+
+namespace DeltaKustoUnitTest.CommandParsing.Policies
+{
+    public class AlterPartitioningPolicyTest : ParsingTestBase
+    {
+        [Fact]
+        public void SimpleTable()
+        {
+            var commandText = @"
+.alter table mydb.mytable policy partitioning ```
+{
+  'PartitionKeys': [
+    {
+                'ColumnName': 'my_string_column',
+      'Kind': 'Hash',
+      'Properties': {
+                    'Function': 'XxHash64',
+        'MaxPartitionCount': 128,
+        'PartitionAssignmentMode': 'Uniform'
+      }
+            }
+  ]
+}```
+";
+            var commands = CommandBase.FromScript(commandText, true);
+
+            Assert.Empty(commands);
+        }
+
+        [Fact]
+        public void DbComposedTableName()
+        {
+            var commandText = @"
+.alter table mydb.mytable policy partitioning ```
+{
+  'PartitionKeys': [
+    {
+                'ColumnName': 'my_string_column',
+      'Kind': 'Hash',
+      'Properties': {
+                    'Function': 'XxHash64',
+        'MaxPartitionCount': 128,
+        'PartitionAssignmentMode': 'Uniform'
+      }
+            }
+  ]
+}```
+";
+            var commands = CommandBase.FromScript(commandText, true);
+
+            Assert.Empty(commands);
+        }
+
+        [Fact]
+        public void ClusterComposedTableName()
+        {
+            var commandText = @"
+.alter table mycluster.mydb.mytable policy partitioning ```
+{
+  'PartitionKeys': [
+    {
+                'ColumnName': 'my_string_column',
+      'Kind': 'Hash',
+      'Properties': {
+                    'Function': 'XxHash64',
+        'MaxPartitionCount': 128,
+        'PartitionAssignmentMode': 'Uniform'
+      }
+            }
+  ]
+}```
+";
+            var commands = CommandBase.FromScript(commandText, true);
+
+            Assert.Empty(commands);
+        }
+    }
+}
