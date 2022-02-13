1e81ee00e097f6540d040983c06095a254b0913d
vplauzon <vincent_lauzon@yahoo.com.au>
2022-02-13 11:27:16 -0500

    Add user-prompt authentication mode

---
+++ code/DeltaKustoIntegration/Parameterization/UserPromptParameterization.cs
@@ -0,0 +1,16 @@
+﻿using DeltaKustoLib;
+using System;
+
+namespace DeltaKustoIntegration.Parameterization
+{
+    public class UserPromptParameterization
+    {
+        public string? TenantId { get; set; }
+
+        public string? UserId { get; set; }
+
+        internal void Validate()
+        {
+        }
+    }
+}
