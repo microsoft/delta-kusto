d0572ef638c19efabd613e66af84b2379f8b75c4
vplauzon <vincent_lauzon@yahoo.com.au>
2023-03-30 19:43:28 -0400

    Add test for no-auth

---
+++ code/DeltaKustoAdxIntegrationTest/NoAuth/NoAuthTest.cs
@@ -0,0 +1,48 @@
+using DeltaKustoIntegration.Database;
+using DeltaKustoLib;
+using DeltaKustoLib.CommandModel;
+using Microsoft.Identity.Client;
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace DeltaKustoAdxIntegrationTest.NoAuth
+{
+    public class NoAuthTest : AdxIntegrationTestBase
+    {
+        public NoAuthTest()
+            //  We do not want the login provider as we are testing the given token provider
+            : base(false)
+        {
+        }
+
+        [Fact]
+        public async Task TestGivenToken()
+        {
+            try
+            {
+                using (var targetDb = await InitializeDbAsync())
+                {
+                    var overrides = ImmutableArray<(string path, string value)>
+                        .Empty
+                        .Add(("jobs.main.target.adx.clusterUri", ClusterUri.ToString()))
+                        .Add(("jobs.main.target.adx.database", targetDb.Name));
+
+                    await PrepareDbAsync("NoAuth/target.kql", targetDb.Name);
+                    await RunParametersAsync("NoAuth/no-auth.yaml", overrides);
+
+                    //  We just test that this doesn't fail
+                }
+                Assert.True(false, "This test is failing");
+            }
+            catch (InvalidOperationException)
+            {
+            }
+        }
+    }
+}
