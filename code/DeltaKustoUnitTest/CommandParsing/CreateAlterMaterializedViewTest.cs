51397770f0ea95a86183671ae7bde6912eb9bcca
vplauzon <vincent_lauzon@yahoo.com.au>
2023-04-04 14:16:19 -0400

    Add MV

---
+++ code/DeltaKustoUnitTest/CommandParsing/CreateAlterMaterializedViewTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace DeltaKustoUnitTest.CommandParsing
+{
+    /// <summary>
+    /// Related to issue https://github.com/microsoft/delta-kusto/issues/91
+    /// </summary>
+    public class CreateAlterMaterializedViewTest
+    {
+        [Fact]
+        public void Something()
+        {
+            Assert.True(false);
+        }
+    }
+}
