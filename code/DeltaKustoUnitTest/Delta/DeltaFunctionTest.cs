4eb0d6555844bafa0429ec3ae91608b2b40ed587
vplauzon <vincent_lauzon@yahoo.com.au>
2023-04-14 17:22:21 -0400

    Bump version

--- code/DeltaKustoUnitTest/Delta/DeltaFunctionTest.cs
+++ code/DeltaKustoUnitTest/Delta/DeltaFunctionTest.cs
@@ -91,6 +91,20 @@ namespace DeltaKustoUnitTest.Delta
         }
 
         [Fact]
+        public void NoUpdateParameterWithDefaultValue()
+        {
+            var currentCommands = Parse(
+                ".create function MyFunction(StartTime:datetime=datetime(null)) { 42 }");
+            var currentDatabase = DatabaseModel.FromCommands(currentCommands);
+            var targetCommands = Parse(
+                ".create function MyFunction(StartTime:datetime=datetime(null)){ 42 }");
+            var targetDatabase = DatabaseModel.FromCommands(targetCommands);
+            var delta = currentDatabase.ComputeDelta(targetDatabase);
+
+            Assert.Empty(delta);
+        }
+
+        [Fact]
         public void DetectDuplicates()
         {
             try
