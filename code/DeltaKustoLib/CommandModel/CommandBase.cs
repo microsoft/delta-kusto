4e1f8819483bb97a3c38023c6350a05d360a5058
vplauzon <vincent_lauzon@yahoo.com.au>
2023-10-19 17:39:08 -0400

    Add delete

--- code/DeltaKustoLib/CommandModel/CommandBase.cs
+++ code/DeltaKustoLib/CommandModel/CommandBase.cs
@@ -191,6 +191,8 @@ namespace DeltaKustoLib.CommandModel
                         return DeleteStreamingIngestionPolicyCommand.FromCode(commandBlock);
                     case "AlterTablePolicyIngestionTime":
                         return AlterIngestionTimePolicyCommand.FromCode(commandBlock);
+                    case "DeleteTablePolicyIngestionTime":
+                        return DeleteIngestionTimePolicyCommand.FromCode(commandBlock);
                     #endregion
 
                     default:
---
+++ code/DeltaKustoUnitTest/CommandParsing/Policies/DeleteIngestionTimePolicyTest.cs
@@ -0,0 +1,37 @@
+using DeltaKustoLib.CommandModel;
+using DeltaKustoLib.CommandModel.Policies;
+using System;
+using System.Linq;
+using System.Text.Json;
+using Xunit;
+
+namespace DeltaKustoUnitTest.CommandParsing.Policies
+{
+    public class DeleteIngestionTimePolicyTest : ParsingTestBase
+    {
+        [Fact]
+        public void SimpleTable()
+        {
+            TestIngestionTimePolicy("A");
+        }
+
+        [Fact]
+        public void FunkyTable()
+        {
+            TestIngestionTimePolicy("['A- 1']");
+        }
+
+        private void TestIngestionTimePolicy(string tableName)
+        {
+            var commandText = $@"
+.delete table {tableName} policy ingestiontime";
+            var command = ParseOneCommand(commandText);
+
+            Assert.IsType<DeleteIngestionTimePolicyCommand>(command);
+
+            var realCommand = (DeleteIngestionTimePolicyCommand)command;
+
+            Assert.Equal(GetActualTableName(tableName), realCommand.TableName.Name);
+        }
+    }
+}
