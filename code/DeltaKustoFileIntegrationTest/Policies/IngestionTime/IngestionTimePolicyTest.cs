7e89c0f8ef28c76c42a0cde818b8543697583af0
vplauzon <vincent_lauzon@yahoo.com.au>
2023-10-21 17:23:25 -0400

    Add test

---
+++ code/DeltaKustoFileIntegrationTest/Policies/IngestionTime/IngestionTimePolicyTest.cs
@@ -0,0 +1,83 @@
+﻿using DeltaKustoLib.CommandModel.Policies.IngestionTime;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace DeltaKustoFileIntegrationTest.Policies.IngestionTime
+{
+    public class IngestionTimePolicyTest : IntegrationTestBase
+    {
+        [Fact]
+        public async Task NoneToOne()
+        {
+            var paramPath = "Policies/IngestionTime/NoneToOne/delta-params.yaml";
+            var parameters = await RunParametersAsync(paramPath);
+            var outputPath = parameters.Jobs!.First().Value.Action!.FilePath!;
+            var outputCommands = await LoadScriptAsync(paramPath, outputPath);
+
+            Assert.Single(outputCommands);
+
+            var policyCommand = outputCommands
+                .Where(c => c is AlterIngestionTimePolicyCommand)
+                .Cast<AlterIngestionTimePolicyCommand>()
+                .FirstOrDefault();
+
+            Assert.NotNull(policyCommand);
+            Assert.Equal("my-table", policyCommand.TableName.Name);
+            Assert.True(policyCommand.IsEnabled);
+        }
+
+        [Fact]
+        public async Task OneToNone()
+        {
+            var paramPath = "Policies/IngestionTime/OneToNone/delta-params.yaml";
+            var parameters = await RunParametersAsync(paramPath);
+            var outputPath = parameters.Jobs!.First().Value.Action!.FilePath!;
+            var outputCommands = await LoadScriptAsync(paramPath, outputPath);
+
+            Assert.Single(outputCommands);
+
+            var policyCommand = outputCommands
+                .Where(c => c is DeleteIngestionTimePolicyCommand)
+                .Cast<DeleteIngestionTimePolicyCommand>()
+                .FirstOrDefault();
+
+            Assert.NotNull(policyCommand);
+            Assert.Equal("my-table", policyCommand.TableName.Name);
+        }
+
+        [Fact]
+        public async Task OneToOne()
+        {
+            var paramPath = "Policies/IngestionTime/OneToOneNoChange/delta-params.yaml";
+            var parameters = await RunParametersAsync(paramPath);
+            var outputPath = parameters.Jobs!.First().Value.Action!.FilePath!;
+            var outputCommands = await LoadScriptAsync(paramPath, outputPath);
+
+            Assert.Empty(outputCommands);
+        }
+
+        [Fact]
+        public async Task OneToOneWithChange()
+        {
+            var paramPath = "Policies/IngestionTime/OneToOneWithChange/delta-params.yaml";
+            var parameters = await RunParametersAsync(paramPath);
+            var outputPath = parameters.Jobs!.First().Value.Action!.FilePath!;
+            var outputCommands = await LoadScriptAsync(paramPath, outputPath);
+
+            Assert.Single(outputCommands);
+
+            var policyCommand = outputCommands
+                .Where(c => c is AlterIngestionTimePolicyCommand)
+                .Cast<AlterIngestionTimePolicyCommand>()
+                .FirstOrDefault();
+
+            Assert.NotNull(policyCommand);
+            Assert.Equal("my-table", policyCommand.TableName.Name);
+            Assert.False(policyCommand.IsEnabled);
+        }
+    }
+}
