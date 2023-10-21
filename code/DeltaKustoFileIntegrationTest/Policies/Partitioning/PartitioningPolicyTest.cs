f9878b14846a40556053db466f197ef9a32d714f
vplauzon <vincent_lauzon@yahoo.com.au>
2023-10-21 17:14:39 -0400

    Add test

---
+++ code/DeltaKustoFileIntegrationTest/Policies/Partitioning/PartitioningPolicyTest.cs
@@ -0,0 +1,81 @@
+﻿using DeltaKustoLib.CommandModel.Policies.Partitioning;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace DeltaKustoFileIntegrationTest.Policies.Partitioning
+{
+    public class PartitioningPolicyTest : IntegrationTestBase
+    {
+        [Fact]
+        public async Task NoneToOne()
+        {
+            var paramPath = "Policies/Partitioning/NoneToOne/delta-params.yaml";
+            var parameters = await RunParametersAsync(paramPath);
+            var outputPath = parameters.Jobs!.First().Value.Action!.FilePath!;
+            var outputCommands = await LoadScriptAsync(paramPath, outputPath);
+
+            Assert.Single(outputCommands);
+
+            var policyCommand = outputCommands
+                .Where(c => c is AlterPartitioningPolicyCommand)
+                .Cast<AlterPartitioningPolicyCommand>()
+                .FirstOrDefault();
+
+            Assert.NotNull(policyCommand);
+            Assert.Equal("my-table", policyCommand!.TableName.Name);
+        }
+
+        [Fact]
+        public async Task OneToNone()
+        {
+            var paramPath = "Policies/Partitioning/OneToNone/delta-params.yaml";
+            var parameters = await RunParametersAsync(paramPath);
+            var outputPath = parameters.Jobs!.First().Value.Action!.FilePath!;
+            var outputCommands = await LoadScriptAsync(paramPath, outputPath);
+
+            Assert.Single(outputCommands);
+
+            var policyCommand = outputCommands
+                .Where(c => c is DeletePartitioningPolicyCommand)
+                .Cast<DeletePartitioningPolicyCommand>()
+                .FirstOrDefault();
+
+            Assert.NotNull(policyCommand);
+            Assert.Equal("my-table", policyCommand!.TableName.Name);
+        }
+
+        [Fact]
+        public async Task OneToOne()
+        {
+            var paramPath = "Policies/Partitioning/OneToOneNoChange/delta-params.yaml";
+            var parameters = await RunParametersAsync(paramPath);
+            var outputPath = parameters.Jobs!.First().Value.Action!.FilePath!;
+            var outputCommands = await LoadScriptAsync(paramPath, outputPath);
+
+            Assert.Empty(outputCommands);
+        }
+
+        [Fact]
+        public async Task OneToOneWithChange()
+        {
+            var paramPath = "Policies/Partitioning/OneToOneWithChange/delta-params.yaml";
+            var parameters = await RunParametersAsync(paramPath);
+            var outputPath = parameters.Jobs!.First().Value.Action!.FilePath!;
+            var outputCommands = await LoadScriptAsync(paramPath, outputPath);
+
+            Assert.Single(outputCommands);
+
+            var policyCommand = outputCommands
+                .Where(c => c is AlterPartitioningPolicyCommand)
+                .Cast<AlterPartitioningPolicyCommand>()
+                .FirstOrDefault();
+
+            Assert.NotNull(policyCommand);
+            Assert.Equal("my-table", policyCommand!.TableName.Name);
+        }
+    }
+}
